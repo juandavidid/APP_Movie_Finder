a2364fe8aaee6674035fe95166044bbcab457995
Juan David <juandavidcarrillogarcia@gmail.com>
2025-03-12 03:17:05 -0500

    Pantalla principal: 1  Maquetacion  de APP Movie Finder

---
+++ components/icons_bottom_index/icons_bottom_index.js
@@ -0,0 +1,11 @@
+Component({
+  mixins: [],
+  data: {
+    typeIcons:[ "UpOutline",'SearchOutline','StarOutline'],  // array para recorre iconos y que se multiple la estructura
+  },
+  props: {},
+  didMount() {},
+  didUpdate() {},
+  didUnmount() {},
+  methods: {},
+});
---
+++ components/movies_popularity/movies_popularity.js
@@ -0,0 +1,12 @@
+Component({
+  mixins: [],
+  data: {
+    arrayCategoryPopularity:["Popular en Netflix","Tendencia ahora","Peliculas Vistas"],
+    imagesMovies:[1,2,3,4,5,7,8],
+  },
+  props: {},
+  didMount() {},
+  didUpdate() {},
+  didUnmount() {},
+  methods: {},
+});
